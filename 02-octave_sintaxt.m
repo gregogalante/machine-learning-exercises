1981d1924c452ec257fce204e1a32dea8e06d7f6
Gregorio Galante <me@gregoriogalante.com>
2020-08-30 12:25:29 +0200

    Update generale

---
+++ 02-octave_sintaxt.m
@@ -0,0 +1,8 @@
+% MATRIX EXAMPLE
+A = [1 2; 3 4; 5 6]
+B = ones(2,3) % creazione di una matrice di valori 1 di dimensione 2 * 3
+C = zeros(2, 3) % creazione di una matrice di valori 0 di dimensione 2 * 3
+D = rand(2, 3) % creazione di una matrice di valori randomici di dimensione 2 * 3
+
+% VISUALIZZAZIONE DATI
+hist(A)
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Machine Learning Exercises
 
 Some exercises about machine learning form the [Coursera Machine Learning Course](https://www.coursera.org/learn/machine-learning).
+
+## Programming languages
+
+1. Python 3.6
+2. Octave (https://www.gnu.org/software/octave)
